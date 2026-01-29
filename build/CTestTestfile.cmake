efbb8ef6905798267841854c2c68b09c6457c4b3
maskedsyntax <aftaab@aftaab.xyz>
2026-01-30 00:39:10 +0530

    Remove build directory and xameditor from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/batman/maskedsyntax/xenon
-# Build directory: /home/batman/maskedsyntax/xenon/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
